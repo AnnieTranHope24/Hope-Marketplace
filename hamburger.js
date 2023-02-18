d0b0422ff77584ae0a2df7bc0d44f86268aa15ab
Bereket Bessie <bereket.bessie@hope.edu>
2023-02-18 12:45:46 -0500

    made header and navbar responsive, created new drop-down menu

--- hamburger.js
+++ hamburger.js
@@ -1,13 +1,15 @@
 const bar1 = document.querySelector(".bar1");
 const bar2 = document.querySelector(".bar2");
 const bar3 = document.querySelector(".bar3");
+const aca = document.querySelector(".aca");
 const burger = document.querySelector(".menu");
 const sidebar = document.querySelector(".sidebar");
 
-burger.addEventListener("click", () => {
-    bar1.classList.toggle("change1");
-    bar2.classList.toggle("change2");
-    bar3.classList.toggle("change3");
+burger.addEventListener("hover", () => {
     sidebar.classList.toggle("reveal");
     burger.classList.toggle(".changemenu");
+});
+
+aca.addEventListener("hover", () => {
+
 });
