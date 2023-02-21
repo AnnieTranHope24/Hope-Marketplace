ebfc6e97687b1d5f8be65cd19e7365d2dbabaa8e
Bereket Bessie <bereket.bessie@hope.edu>
2023-02-21 02:11:34 -0500

    Made about us page & made the footer

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,15 +1,19 @@
-const bar1 = document.querySelector(".bar1");
-const bar2 = document.querySelector(".bar2");
-const bar3 = document.querySelector(".bar3");
-const aca = document.querySelector(".aca");
-const burger = document.querySelector(".menu");
-const sidebar = document.querySelector(".sidebar");
 
-burger.addEventListener("hover", () => {
-    sidebar.classList.toggle("reveal");
-    burger.classList.toggle(".changemenu");
-});
+function details(){
+  const x = document.querySelector(".more-details");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none"
+    }
+  }
 
-aca.addEventListener("hover", () => {
-
-});
+  function details1(){
+    const x = document.querySelector(".more-details1");
+    const y = document.querySelector(".botton1");
+      if (x.style.display === "none") {
+        x.style.display = "flex";
+      } else {
+        x.style.display = "none"
+      }
+    }
