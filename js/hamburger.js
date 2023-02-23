ee47e3c066b935658985ec990e2d9d5c6b2300b8
Bereket Bessie <bereket.bessie@hope.edu>
2023-02-23 09:26:16 -0500

    completed hamburger menu bar

--- js/hamburger.js
+++ js/hamburger.js
@@ -28,11 +28,11 @@ function details(){
     }
 
 const hamburger = document.querySelector(".hamburger");
-//const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
-   // navMenu.classList.toggle("active");
+    navMenu.classList.toggle("active");
 }
