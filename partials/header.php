21b882707c33cf6b59e31f6e0158b291dff2ded0
Annie <ngoc.tran@hope.edu>
2023-04-12 12:10:10 -0400

    Change the header

--- partials/header.php
+++ partials/header.php
@@ -44,7 +44,25 @@
           while($row_data = mysqli_fetch_assoc($result_cat)){
             $cat_title = $row_data['cat_title'];
             $cat_id = $row_data['cat_id'];
-            echo '<a class="nav-link" href="academics.php"><img src=images/icons/book-icon.svg>'.$cat_title.'</a>';
+            if($cat_title == "Academic"){
+              echo '<a class="nav-link" href="academics.php"><img src=images/icons/book-icon.svg>'.$cat_title.'</a>';
+            }
+            elseif($cat_title == "Health"){
+              echo '<a class="nav-link" href="health.php"><img src=images/icons/Weights.svg>Health</a>';
+            }
+            elseif($cat_title == "Fashion"){
+              echo '<a class="nav-link" href="fashion.php"><img src=images/icons/Cloth.svg>Fashion</a>';
+            }
+            elseif($cat_title == "Room"){
+              echo '<a class="nav-link" href="room.php"><img src=images/icons/Lamp.svg>Room</a>';
+            }
+            elseif($cat_title == "Entertainment"){
+              echo '<a class="nav-link" href="entertainment.php"><img src=images/icons/TV.svg>Entertainment</a>';
+            } 
+            else{
+              echo '<a class="nav-link" href="academics.php"><img src=images/icons/book-icon.svg>'.$cat_title.'</a>';
+            }           
+            
           }
         ?>              
         <!-- <a class="nav-link" href="academics.php"><img src=images/icons/book-icon.svg> Academics</a>
@@ -70,138 +88,163 @@
           while($row_data = mysqli_fetch_assoc($result_cat)){
             $cat_title = $row_data['cat_title'];
             $cat_id = $row_data['cat_id'];
-            echo '      <div class="dropdown">        
-            <a id="category" href="academics.php">'.$cat_title.'</a>
-            <div class="dropdown-content">
-              <div>
-                <img src="images/icons/resources.png" alt="">
-                <h3>Resources</h3>
-                <a href="/academics.php#textbooks"> Textbooks</a>
-                <a href="/academics.php#books"> Books</a>
-                <a href="/academics.php#testprep"> Test Prep</a>
+            if($cat_title == "Academic"){
+              echo '      <div class="dropdown">        
+              <a id="category" href="academics.php">'.$cat_title.'</a>
+              <div class="dropdown-content">
+                <div>
+                  <img src="images/icons/resources.png" alt="">
+                  <h3>Resources</h3>
+                  <a href="/academics.php#textbooks"> Textbooks</a>
+                  <a href="/academics.php#books"> Books</a>
+                  <a href="/academics.php#testprep"> Test Prep</a>
+                </div>
+                <div>
+                  <img src="images/icons/supplies.png" alt="">
+                  <h3>Supplies</h3>
+                  <a href="#"> Folder</a>
+                  <a href="#"> Pens & Pencils</a>
+                  <a href="#"> Binders</a>
+                  <a href="#"> Note Cards</a>
+                </div>
               </div>
-              <div>
-                <img src="images/icons/supplies.png" alt="">
-                <h3>Supplies</h3>
-                <a href="#"> Folder</a>
-                <a href="#"> Pens & Pencils</a>
-                <a href="#"> Binders</a>
-                <a href="#"> Note Cards</a>
+            </div>';
+            }
+            elseif ($cat_title == "Health"){
+              echo '      <div class="dropdown">
+              <a id="category" href="health.php">'.$cat_title.'</a>
+              <div class="dropdown-content">
+                <div>
+                  <img class="catagory-img" src="images/icons/gym.png" alt="">
+                  <h3>Gym Equipment</h3>
+                  <a href="#"> Home Workout Equipment</a>
+                  <a href="#"> Fitness Clothes</a>
+                  <a hrer="#"> Weightlifting Belts</a>
+                  <a href="#"> Foam Rollers</a>
+                  <a href="#"> Sport Equipment</a>
+                </div>
+                <div>
+                  <img src="images/icons/yoga.png" alt="">
+                  <h3>Recovery</h3>
+                  <a href="#"> Yoga Matts</a>
+                  <a href="#"> Waterbottles</a>
+                  <a hrer="#"> Bodyweight Scales</a>
+                  <a href="#"> Massage Guns</a>
+                </div>
               </div>
-            </div>
-          </div>';
+            </div>';
+            }
+  
+            elseif($cat_title == 'Room'){
+              echo '      <div class="dropdown">
+              <a id="category" href="room.php">Room</a>
+              <div class="dropdown-content">
+                <div>
+                  <img src="images/icons/sofa.png" alt="">
+                  <h3>Room Decorations</h3>
+                  <a href="#"> Plants</a>
+                  <a href="#"> Lights</a>
+                  <a href="#"> Rugs</a>
+                  <a href="#"> Art</a>
+                </div>
+                <div>
+                  <img src="images/icons/cutlery.png" alt="">
+                  <h3>Essientals</h3>
+                  <a href="#">Plates & Bowls</a>
+                  <a href="#"> Utensils</a>
+                  <a href="#"> Water Heaters</a>
+                  <a href="#"> Coffee Machines</a>
+                  <a href="#"> Pots & Pans</a>
+                </div>
+              </div>
+            </div>';
+            }
+  
+            elseif($cat_title == 'Fashion'){
+              echo '      <div class="dropdown">
+              <a id="category" href="fashion.php">Fashion</a>
+              <div class="dropdown-content">
+                <div>
+                  <h3>Mens</h3>
+                  <a href="#"> Clothing</a>
+                  <a href="#"> Sports Fan Shop</a>
+                  <a href="#"> Shoes</a>
+                </div>
+                <div>
+                  <h3>Womens</h3>
+                  <a href="#"> Clothing</a>
+                  <a href="#"> Sports Fan Shop</a>
+                  <a href="#"> Shoes</a>
+                </div>
+                <div>
+                  <h3>Accessories</h3>
+                  <a href="#"> Handbagsn & BackPacks</a>
+                  <a href="#"> Sunglassess</a>
+                  <a href="#"> Hats</a>
+                  <a href="#"> Jewelery & Watches</a>
+                </div>
+              </div>
+            </div>';
+            }
+  
+            elseif($cat_title == 'Entertainment'){
+              echo '      <div class="dropdown">
+              <a id="category" href="entertainment.php">Entertainment</a>
+              <div class="dropdown-content">
+                <div>
+                  <h3>Technology</h3>
+                  <a href="#"> Computers & Tablets</a>
+                  <a href="#"> TV & Home Theater</a>
+                  <a href="#"> Cameras</a>
+                  <a href="#"> Speakers</a>
+                </div>
+                <div>
+                  <h3>Wearable Tech</h3>
+                  <a href="#"> Smart Watches</a>
+                  <a href="#"> Headphones</a>
+                  <a href="#"> Phones</a>
+                </div>
+                <div>
+                  <h3>Entertainment</h3>
+                  <a href="#"> Video Games</a>
+                  <a href="#"> Movies & Music</a>
+                  <a href="#"> Collectibles</a>
+                </div>
+              </div>
+            </div>';
+            }
+            else{
+              echo '      <div class="dropdown">
+              <a id="category" href="health.php">'.$cat_title.'</a>
+              <div class="dropdown-content">
+                <div>
+                  <img class="catagory-img" src="images/icons/gym.png" alt="">
+                  <h3>Gym Equipment</h3>
+                  <a href="#"> Home Workout Equipment</a>
+                  <a href="#"> Fitness Clothes</a>
+                  <a hrer="#"> Weightlifting Belts</a>
+                  <a href="#"> Foam Rollers</a>
+                  <a href="#"> Sport Equipment</a>
+                </div>
+                <div>
+                  <img src="images/icons/yoga.png" alt="">
+                  <h3>Recovery</h3>
+                  <a href="#"> Yoga Matts</a>
+                  <a href="#"> Waterbottles</a>
+                  <a hrer="#"> Bodyweight Scales</a>
+                  <a href="#"> Massage Guns</a>
+                </div>
+              </div>
+            </div>';
+            }
           }
+
+
         ?>  
       <!-- <div class="dropdown">        
-        <a id="category" href="academics.php"> Academics </a>
-        <div class="dropdown-content">
-          <div>
-            <img src="images/icons/resources.png" alt="">
-            <h3>Resources</h3>
-            <a href="/academics.php#textbooks"> Textbooks</a>
-            <a href="/academics.php#books"> Books</a>
-            <a href="/academics.php#testprep"> Test Prep</a>
-          </div>
-          <div>
-            <img src="images/icons/supplies.png" alt="">
-            <h3>Supplies</h3>
-            <a href="#"> Folder</a>
-            <a href="#"> Pens & Pencils</a>
-            <a href="#"> Binders</a>
-            <a href="#"> Note Cards</a>
-          </div>
-        </div>
-      </div>
-      <div class="dropdown">
-        <a id="category" href="health.php"> Health </a>
-        <div class="dropdown-content">
-          <div>
-            <img class="catagory-img" src="images/icons/gym.png" alt="">
-            <h3>Gym Equipment</h3>
-            <a href="#"> Home Workout Equipment</a>
-            <a href="#"> Fitness Clothes</a>
-            <a hrer="#"> Weightlifting Belts</a>
-            <a href="#"> Foam Rollers</a>
-            <a href="#"> Sport Equipment</a>
-          </div>
-          <div>
-            <img src="images/icons/yoga.png" alt="">
-            <h3>Recovery</h3>
-            <a href="#"> Yoga Matts</a>
-            <a href="#"> Waterbottles</a>
-            <a hrer="#"> Bodyweight Scales</a>
-            <a href="#"> Massage Guns</a>
-          </div>
-        </div>
-      </div>
-      <div class="dropdown">
-        <a id="category" href="room.php">Room</a>
-        <div class="dropdown-content">
-          <div>
-            <img src="images/icons/sofa.png" alt="">
-            <h3>Room Decorations</h3>
-            <a href="#"> Plants</a>
-            <a href="#"> Lights</a>
-            <a href="#"> Rugs</a>
-            <a href="#"> Art</a>
-          </div>
-          <div>
-            <img src="images/icons/cutlery.png" alt="">
-            <h3>Essientals</h3>
-            <a href="#">Plates & Bowls</a>
-            <a href="#"> Utensils</a>
-            <a href="#"> Water Heaters</a>
-            <a href="#"> Coffee Machines</a>
-            <a href="#"> Pots & Pans</a>
-          </div>
-        </div>
-      </div>
-      <div class="dropdown">
-        <a id="category" href="fashion.php">Fashion</a>
-        <div class="dropdown-content">
-          <div>
-            <h3>Mens</h3>
-            <a href="#"> Clothing</a>
-            <a href="#"> Sports Fan Shop</a>
-            <a href="#"> Shoes</a>
-          </div>
-          <div>
-            <h3>Womens</h3>
-            <a href="#"> Clothing</a>
-            <a href="#"> Sports Fan Shop</a>
-            <a href="#"> Shoes</a>
-          </div>
-          <div>
-            <h3>Accessories</h3>
-            <a href="#"> Handbagsn & BackPacks</a>
-            <a href="#"> Sunglassess</a>
-            <a href="#"> Hats</a>
-            <a href="#"> Jewelery & Watches</a>
-          </div>
-        </div>
-      </div>
-      <div class="dropdown">
-        <a id="category" href="entertainment.php">Entertainment</a>
-        <div class="dropdown-content">
-          <div>
-            <h3>Technology</h3>
-            <a href="#"> Computers & Tablets</a>
-            <a href="#"> TV & Home Theater</a>
-            <a href="#"> Cameras</a>
-            <a href="#"> Speakers</a>
-          </div>
-          <div>
-            <h3>Wearable Tech</h3>
-            <a href="#"> Smart Watches</a>
-            <a href="#"> Headphones</a>
-            <a href="#"> Phones</a>
-          </div>
-          <div>
-            <h3>Entertainment</h3>
-            <a href="#"> Video Games</a>
-            <a href="#"> Movies & Music</a>
-            <a href="#"> Collectibles</a>
-          </div>
-        </div>
-      </div> -->
+
+
+
+
+ -->
     </div>
