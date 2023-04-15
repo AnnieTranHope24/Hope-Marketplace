59537618782d0c9af3878df0c3ac48f4838b8181
Annie <ngoc.tran@hope.edu>
2023-04-15 18:31:49 -0400

    Merge with Bereket's version

--- academics.php
+++ academics.php
@@ -1,193 +1,97 @@
-<!DOCTYPE html>
+<?php require_once('config.php');
+try {
+  $dbh = new PDO(DBCONNSTRING,DBUSER,DBPASS);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo 'Connection failed: ' . $e->getMessage();
+}
+?>
 <html lang="en">
 <head>
-  <?php include './partials/head.php' ?>
+  <meta charset="utf-8">
   <title>Acedemics</title>
+  <link rel="stylesheet" href="css/reset.css" />
+  <link rel="stylesheet" href="css/index.css" />
+  <link rel="stylesheet" href="about.html" />
   <link rel="stylesheet" href="css/academics.css" />
+  <style>
+.itemname {
+  font-size: 16px;
+  white-space: nowrap;
+  width: 200px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  line-height: 1.5;
+}
+</style>
 </head>
 <body>
   <header>
-    <?php include './partials/header.php'?>
+    <?php
+    include "partials/header.php" ;
+    ?>
   </header>
   <body>
     <main>
       <h1 id="textbook">Textbooks</h1>
       <div class="wrapper">
-        <div class="item">
-          <a href="#"><img src="images/academics/textbook1.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/textbook2.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/textbook3.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/textbook4.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/textbook5.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/textbook6.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/textbook7.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/textbook8.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/textbook9.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/textbook10.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
+        <?php
+        $stmt = $dbh->prepare("SELECT * FROM items Where Subcategory='textbook'");
+				$stmt->execute();
+				$items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+				// Display items
+				foreach ($items as $item) {
+          echo '<div class="item">';
+          echo '<a href="#">'. '<img src="UPLOADS/' . $item['Image'] . '"></a>';
+          echo '<p class="itemname">'.$item['Name'].'</p>';
+          echo '<p class="itemprice">$'.$item['Price'].'</p>';
+          echo '<button class="add">Add Item</button>';
+          echo '</div>';
+         }
+        ?>
       </div>
       <h1 id="testprep">Test Preparation</h1>
       <div class="wrapper">
-        <div class="item">
-          <a href="#"><img src="images/academics/testprep.jpg"></a>
-          <p class="item-price"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/testprep1.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/testprep2.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/testprep3.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/testprep4.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/testprep.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/testprep5.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/testprep6.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/testprep7.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/testprep8.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/testprep9.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/testprep10.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
+      <?php
+        $stmt = $dbh->prepare("SELECT * FROM items Where Subcategory='testprep'");
+				$stmt->execute();
+				$items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+				// Display items
+				foreach ($items as $item) {
+         echo '<div class="item">';
+         echo '<a href="#">'. '<img src="UPLOADS/' . $item['Image'] . '"></a>';
+         echo '<p class="itemname">'.$item['Name'].'</p>';
+         echo '<p class="itemprice">$'.$item['Price'].'</p>';
+         echo '<button class="add">Add Item</button>';
+         echo '</div>';
+        }
+        ?>
       </div>
       <h1  class="books">Books</h1>
       <div class="wrapper">
-        <div class="item">
-          <a href="#"><img src="images/academics/book.jpg"></a>
-          <p class="item-price"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/book1.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/book2.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/book3.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/book4.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/book5.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/book6.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/book7.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/book8.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/book9.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
-        <div class="item">
-          <a href="#"><img src="images/academics/book10.jpg"></a>
-          <p class="itemprice"> $10.99</p>
-          <button class="add">Add Item</button>
-        </div>
+      <?php
+        $stmt = $dbh->prepare("SELECT * FROM items Where Subcategory='book'");
+				$stmt->execute();
+				$items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+				// Display items
+				foreach ($items as $item) {
+          echo '<div class="item">';
+          echo '<a href="#">'. '<img src="UPLOADS/' . $item['Image'] . '"></a>';
+          echo '<p class="itemname">'.$item['Name'].'</p>';
+          echo '<p class="itemprice">$'.$item['Price'].'</p>';
+          echo '<button class="add">Add Item</button>';
+          echo '</div>';
+         }
+        ?>
       </div>
     </main>
     <footer>
-      <?php include './partials/footer.php'?>
+         <?php
+          include "partials/footer.php";
+         ?>
     </footer>
     <script src="js/hamburger.js"></script>
   </body>
