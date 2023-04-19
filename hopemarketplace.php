8df1a595f2b23a8bce66443487859b8d88e65d43
Bereket Bessie <bereket.bessie@hope.edu>
2023-04-19 18:34:25 -0400

    fixed some error with mysql requests

--- hopemarketplace.php
+++ hopemarketplace.php
@@ -13,17 +13,6 @@ if (isset($_GET['logout'])) {
   header('Location: login.php'); // Redirect to the login page or any other desired page
   exit;
 }
-require_once('config.php');
-
-include 'categories.php';
-
-try {
-  $dbh = new PDO(DBCONNSTRING, DBUSER, DBPASS);
-  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-  echo 'Connection failed: ' . $e->getMessage();
-}
-
 ?>
 
 <!DOCTYPE php>
@@ -35,6 +24,7 @@ try {
     <title>Hope Marketplace</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js"></script>
+
   </head>
 
   <body>
@@ -49,58 +39,79 @@ try {
         <main>
           <!-- Annie Tran - Carousel using Tiny Slider  -->
           <?php
+          require_once('config.php');
+
+          include 'categories.php';
+          
+          try {
+            $pdo = new PDO(DBCONNSTRING, DBUSER, DBPASS);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
+          }
           if (isset($_POST['search'])) {
             $name = $_POST['search'];
-            $stmt = $dbh->prepare("SELECT * FROM items Where Name LIKE '%$name%'");
+            $stmt = $pdo->prepare("SELECT * FROM items WHERE Name LIKE CONCAT('%', :name, '%')");
+            $stmt->bindParam(':name', $name);
             $stmt->execute();
             $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
             echo '<div class="wrapper2">';
+            if (!empty($items)) {
             foreach ($items as $item) {
               echo '<div class="item" onclick="fill(\'' . $item['Name'] . $item['Price'] . $item['Image'] . '\')">' . '<a href="#">' . '<img src="UPLOADS/' . $item['Image'] . '"></a>';
               echo '<p class="itemname">' . $item['Name'] . '</p>';
               echo '<p class="itemprice">$' . $item['Price'] . '</p>';
               echo '<button class="add">Add Item</button>';
               echo '</div>';
+            }}else {
+              echo '<h1>No Results</h1>';
             }
             echo '</div>';
           } elseif (isset($_GET['subcategory'])) {
             $name = lcfirst($_GET['subcategory']);
-            $stmt = $dbh->prepare("SELECT * FROM items Where Subcategory= '$name'");
+            $stmt = $pdo->prepare("SELECT * FROM items WHERE Subcategory = ?");
+            $stmt->bindParam(1,$name, PDO::PARAM_STR);
             $stmt->execute();
             $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            echo '<h1>' . $_GET['subcategory'] . '</h1>';
-            echo '<div class="wrapper2">';
-            // Display items
-            foreach ($items as $item) {
+            if (!empty($items)) {
+              echo '<h1>' . $_GET['subcategory'] . '</h1>';
+              echo '<div class="wrapper2">';
+              foreach($items as $item){
               echo '<div class="item">';
               echo '<a href="#">' . '<img src="UPLOADS/' . $item['Image'] . '"></a>';
               echo '<p class="itemname">' . $item['Name'] . '</p>';
               echo '<p class="itemprice">$' . $item['Price'] . '</p>';
               echo '<button class="add">Add Item</button>';
               echo '</div>';
+            } }else {
+              echo '<h1>No Results</h1>';
             }
-            echo '</div>';
+            echo '</div>';            
           } elseif (isset($_GET['category'])) {
             foreach ($categories as $category) {
               if ($category['category'] == $_GET['category']) {
                 foreach ($category['subcategories'] as $sub) {
                   foreach ($sub as $subcategory) {
                     $name = lcfirst($subcategory);
-                    $stmt = $dbh->prepare("SELECT * FROM items Where Subcategory= '$name'");
+                    $stmt = $pdo->prepare("SELECT * FROM items Where Subcategory= ?");
+                    $stmt->bindParam(1,$name, PDO::PARAM_STR);
                     $stmt->execute();
                     $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                    echo '<h1>' . $subcategory . '</h1>';
-                    echo '<div class="wrapper">';
-                    // Display items
-                    foreach ($items as $item) {
+                    if (!empty($items)) {
+                      echo '<h1>' . $subcategory . '</h1>';
+                      echo '<div class="wrapper2">';
+                      foreach($items as $item){
                       echo '<div class="item">';
                       echo '<a href="#">' . '<img src="UPLOADS/' . $item['Image'] . '"></a>';
                       echo '<p class="itemname">' . $item['Name'] . '</p>';
                       echo '<p class="itemprice">$' . $item['Price'] . '</p>';
                       echo '<button class="add">Add Item</button>';
                       echo '</div>';
+                    } }else {
+                      echo '<h1>No Results</h1>';
                     }
-                    echo '</div>';
+                    echo '</div>';            
                   }
                 }
               }
