8e7134744548d7a08913be3ee39e652576a1049e
Bereket Bessie <bereket.bessie@hope.edu>
2023-04-23 21:11:47 -0400

    index

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
+<?php
+session_start();
 
+$_SESSION['ref-time'] = time();
+// Check if the user is not logged in
+if (!isset($_SESSION['username'])) {
+  // Redirect to the login page
+  header('Location: login.php');
+  exit;
+}
+
+if (isset($_GET['logout'])){
+  session_unset(); // Unset all session variables
+  session_destroy(); // Destroy the session
+  header('Location: login.php'); // Redirect to the login page or any other desired page
+  exit;
+}
+?>
 <!DOCTYPE php>
 
 <php lang="en">
